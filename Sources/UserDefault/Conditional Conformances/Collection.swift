a5e36e94d520e946efc24d3136883c088c6286f3
Julian Dunskus <julian.dunskus@gmail.com>
2019-08-02 01:52:53 +0200

    added conformance framework for collections
    (comes pre-packaged with support for Set)

---
+++ Sources/UserDefault/Conditional Conformances/Collection.swift
@@ -0,0 +1,36 @@
+// Created by Julian Dunskus
+
+import Foundation
+
+public protocol ExpressibleByArray: Sequence {
+	associatedtype Element
+	
+	init(array: [Element])
+}
+
+public protocol ExpressibleBySequence: ExpressibleByArray {
+	init<S>(_ sequence: S) where S: Sequence, S.Element == Element
+}
+
+public extension ExpressibleBySequence {
+	init(array: [Element]) {
+		self.init(array)
+	}
+}
+
+extension DefaultsValueConvertible where
+	Self: ExpressibleByArray, Element: DefaultsValueConvertible
+{
+	public init(defaultsRepresentation: [Element.DefaultsRepresentation]) throws {
+		self.init(array: try defaultsRepresentation.map(Element.init(defaultsRepresentation:)))
+	}
+	
+	public func defaultsRepresentation() throws -> [Element.DefaultsRepresentation] {
+		return try self.map { try $0.defaultsRepresentation() }
+	}
+}
+
+extension Set: ExpressibleBySequence {}
+extension Set: DefaultsValueConvertible where Element: DefaultsValueConvertible {
+	public typealias DefaultsRepresentation = [Element.DefaultsRepresentation]
+}
