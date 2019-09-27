71d084b40bda48f66a6904505c8a6de919a6a98b
Julian Dunskus <julian.dunskus@gmail.com>
2019-09-27 22:38:39 +0200

    made compatible with tvOS and watchOS

--- Sources/UserDefault/Conditional Conformances/NSCoding.swift
+++ Sources/UserDefault/Conditional Conformances/NSCoding.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension DefaultsValueConvertible where Self: NSObject & NSCoding {
 	func defaultsRepresentation() throws -> Data {
-		if #available(macOS 10.13, iOS 11.0, *) {
+		if #available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *) {
 			return try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: false)
 		} else {
 			return NSKeyedArchiver.archivedData(withRootObject: self)
@@ -11,7 +11,7 @@ public extension DefaultsValueConvertible where Self: NSObject & NSCoding {
 	
 	init(defaultsRepresentation: Data) throws {
 		let object: Self?
-		if #available(macOS 10.13, iOS 11.0, *) {
+		if #available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *) {
 			object = try NSKeyedUnarchiver.unarchivedObject(ofClass: Self.self, from: defaultsRepresentation)
 		} else {
 			object = NSKeyedUnarchiver.unarchiveObject(with: defaultsRepresentation) as? Self
